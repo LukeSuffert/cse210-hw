e06c2c333e9df85213a701f777fdc628b5082d62
lukesuffert <lukesuffert@byui.edu>
2026-01-22 11:19:08 -0700

    Learning 2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job Job1 = new Job();
+        Job1._company = "Google";
+        Job1._jobTitle = "Software Engineer";
+        Job1._startYear = 2027;
+        Job1._endYear = 2030;
+
+        Job Job2 = new Job();
+        Job2._company = "Tesla";
+        Job2._jobTitle = "CEO";
+        Job2._startYear = 2030;
+        Job2._endYear = 2050;
+
+        Job1.DisplayJobDetails();
+        Job2.DisplayJobDetails();
+
+        Resume myresume = new Resume();
+        myresume._name = "Luke Suffert";
+
+        myresume._jobs.Add(Job1);
+        myresume._jobs.Add(Job2);
+
+        myresume.DisplayResume();
+
     }
 }
